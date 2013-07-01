e3777d12a384807d9bf869ec3b0de097e56a6b76
Dwayne Anderson <dwayne.anderson@gmail.com>
2013-07-01 15:25:48 -0700

    it works!

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
   var num_clicks = 0;
   var selected = [];
 
-  var is_match = function() {
+  var is_current_selection_a_match = function() {
     var img1src = selected[0].find('img').attr('src');
     var img2src = selected[1].find('img').attr('src');
     return (img1src == img2src);
@@ -12,7 +12,7 @@ $(document).ready(function(){
 
   var handle_click = function() {
     var tile = $(this);
-    if (tile.hasClass('active')) {
+    if (tile.hasClass('active') || tile.hasClass('matched')) {
       return false;
     }
     if (selected.length == 2) {
@@ -22,8 +22,14 @@ $(document).ready(function(){
     num_clicks++;
     tile.addClass('active');
     selected.push(tile);
-    if (selected.length == 2 && is_match()) {
-      alert('match');
+    if (selected.length == 2 && is_current_selection_a_match()) {
+      $.each(selected, function(index, matched_tile) {
+        matched_tile.addClass('matched');
+      });
+      if ($('.tile.matched').size() == tiles.length) {
+        alert('Good Job! You found all the matches in '+num_clicks+' clicks!');
+        window.location.href = window.location.href;
+      }
     }
   }
 
